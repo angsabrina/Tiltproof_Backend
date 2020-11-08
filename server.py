81218a36d94bec8f50b926d754480166136d8c01
Julie Wang <julie.wang@emory.edu>
2020-11-08 00:33:06 -0500

    graph images creation and serving path
    added functions to create graph images for each summoner and path to serve the images for that summoner

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, json
+from flask import Flask, json, send_from_directory
 from flask_cors import CORS, cross_origin
 import random
 import os
@@ -220,6 +220,186 @@ def get_data(pid, accountid, watcher):
       print(e)
       return None
 
+
+# --- Graphs Outputs
+from plotnine import ggplot, aes, geom_point, geom_line, geom_smooth, scale_x_reverse, geom_text, facet_wrap, xlab, ylab, ylim, ggtitle
+def same_day(time_1, time_2):
+    time_1 /= 1000
+    time_2 /= 1000
+    
+    evaluate_1 = datetime.datetime.utcfromtimestamp(time_1).strftime('%Y-%m-%d')
+    evaluate_2 = datetime.datetime.utcfromtimestamp(time_2).strftime('%Y-%m-%d')
+    
+    return evaluate_1 == evaluate_2
+
+def graph_get_data(pid, accountid):
+    '''
+    Given player idea, generate a single row of data
+    '''
+    region = 'na1'
+    mapping_list = [] # stores all mappings
+    
+    try:
+        solo_queue_rank_info = [x for x in watcher.league.by_summoner(region, pid) if x['queueType'] == 'RANKED_SOLO_5x5'][0]  # query 1
+
+        # compute match features #
+        matches = watcher.match.matchlist_by_account(region, accountid)
+        eval_day = None
+        idx = 0
+
+        for game in matches['matches']:
+            print ('idx = ', idx)
+            mapping = {}
+            if idx == 0:
+                # gen label #
+                eval_day = game['timestamp']
+                game_id = game['gameId']
+                match_detail = watcher.match.by_id(region, game_id)
+                game_length_sec=match_detail['gameDuration']
+                participant_id = [x for x in match_detail['participantIdentities'] if x['player']['accountId'] == accountid][0]['participantId']
+                match_features = [x for x in match_detail['participants'] if x['participantId'] == participant_id][0]
+                label = match_features['stats']['win']
+
+                idx+=1
+            elif idx < 6:
+                if same_day(game['timestamp'], eval_day):
+                    # same day, record this data
+                    game_id = game['gameId']
+                    match_detail = watcher.match.by_id(region, game_id)
+                    participant_id = [x for x in match_detail['participantIdentities'] if x['player']['accountId'] == accountid][0]['participantId']
+                    match_features = [x for x in match_detail['participants'] if x['participantId'] == participant_id][0]
+
+                    win = match_features['stats']['win']
+
+                    mapping['win'] = match_features['stats']['win']
+                    mapping['kills'] = match_features['stats']['kills']
+                    mapping['deaths'] = match_features['stats']['deaths']
+                    mapping['assists'] = match_features['stats']['assists']
+                    mapping['kda'] = match_features['stats']['kills'] + match_features['stats']['assists'] / match_features['stats']['deaths']
+                    mapping['totalDamageDealtToChampions'] = match_features['stats']['totalDamageDealtToChampions']
+                    mapping['magicDamageDealtToChampions'] = match_features['stats']['magicDamageDealtToChampions']
+                    mapping['physicalDamageDealtToChampions'] = match_features['stats']['physicalDamageDealtToChampions']
+
+                    mapping['TotalDamageDealtToChampionsPerMinute'] = match_features['stats']['totalDamageDealtToChampions']/game_length_sec*60
+                    mapping['MagicDamageDealtToChampionsPerMinute'] = match_features['stats']['magicDamageDealtToChampions']/game_length_sec*60
+                    mapping['PhysicalDamageDealtToChampionsPerMinute'] = match_features['stats']['physicalDamageDealtToChampions']/game_length_sec*60
+
+                    mapping['visionScore'] = match_features['stats']['visionScore']
+
+                    mapping['goldEarned'] = match_features['stats']['goldEarned']
+                    mapping['goldEarnedPerMinute'] = match_features['stats']['goldEarned']/game_length_sec*60
+
+                    mapping['totalMinionsKilled'] = match_features['stats']['totalMinionsKilled']
+                    mapping['totalMinionsKilledPerMinute'] = match_features['stats']['totalMinionsKilled']/game_length_sec*60
+
+                    mapping['role'] = match_features['timeline']['role']
+                    mapping['lane'] = match_features['timeline']['lane']
+
+                    mapping_list.append(mapping)
+
+                    idx+=1
+                elif idx == 1:
+                    # if we are looking at the second game, and it is not in the same day, we have no good data. Do not record anything
+                    # reset and start over
+                    eval_day = game['timestamp']
+                    idx = 0
+                else:
+                    # no data past g-2, replace with -1
+                    if idx < 5:
+                        mapping['win'] = -1
+                        mapping['kills'] = -1
+                        mapping['deaths'] = -1
+                        mapping['assists'] = -1
+                        mapping['kda'] = -1
+                        mapping['totalDamageDealtToChampions'] = -1
+                        mapping['magicDamageDealtToChampions'] = -1
+                        mapping['physicalDamageDealtToChampions'] = -1
+
+                        mapping['TotalDamageDealtToChampionsPerMinute'] = -1
+                        mapping['MagicDamageDealtToChampionsPerMinute'] = -1
+                        mapping['PhysicalDamageDealtToChampionsPerMinute'] = -1
+
+                        mapping['visionScore'] = -1
+
+                        mapping['goldEarned'] = -1
+                        mapping['goldEarnedPerMinute'] = -1
+
+                        mapping['totalMinionsKilled'] = -1
+                        mapping['totalMinionsKilledPerMinute'] = -1
+
+                        mapping['role'] = -1
+                        mapping['lane'] = -1
+                    idx+=1
+                    mapping_list.append(mapping)
+            else:
+                # we only care about past 5 games. If we reach here, reset
+                break
+
+        return mapping_list
+
+    except Exception as e: 
+        print(e)
+        print("no solo queue info")
+        return None
+
+def graph_process_player():
+
+    return
+
+@api.route('/dummy/<summoner>', methods=['GET'])
+def graph_get_history(summoner):
+    player = watcher.summoner.by_name('na1', summoner)
+    mapping_list = graph_get_data(player['id'], player['accountId'])
+    print(mapping_list)
+    master_list = []
+    master_list.extend(mapping_list)
+    df=pd.DataFrame(master_list)
+    # Fix Support & ADC Labels
+    df.loc[df.role == 'DUO_SUPPORT', 'lane'] = "SUPPORT"
+    df.loc[df.role == 'DUO_CARRY', 'lane'] = "ADC"
+    # Create Column Games_Ago for X Axis Ordering
+    df['games_ago'] = df.reset_index().index+1
+    kda = graph_kda(df)
+    kda.save(filename = f'./static/images/{summoner}_kda.jpg', height=3, width=5, units = 'in', dpi=1000)
+    tddtc = graph_tddtc(df)
+    tddtc.save(filename = f'./static/images/{summoner}_tddtc.jpg', height=3, width=5, units = 'in', dpi=1000)
+    mddtc = graph_mddtc(df)
+    mddtc.save(filename = f'./static/images/{summoner}_mddtc.jpg', height=3, width=5, units = 'in', dpi=1000)
+    pddtc = graph_pddtc(df)
+    pddtc.save(filename = f'./static/images/{summoner}_pddtc.jpg', height=3, width=5, units = 'in', dpi=1000)
+    gepm = graph_gepm(df)
+    gepm.save(filename = f'./static/images/{summoner}_gepm.jpg', height=3, width=5, units = 'in', dpi=1000)
+    tmk = graph_tmk(df)
+    tmk.save(filename = f'./static/images/{summoner}_tmk.jpg', height=3, width=5, units = 'in', dpi=1000)
+    return 'Files Saved!'
+
+def graph_kda(df):
+    kda = ggplot(df) + aes(x='games_ago', y='kda') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + ylim(0,30) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('KDA') + ggtitle("KDA Over the Last 5 Games")
+    return kda
+
+def graph_tddtc(df):
+    tddtc = ggplot(df) + aes(x='games_ago', y='TotalDamageDealtToChampionsPerMinute') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('Total Damage Dealt To Champions \n Per Minute') + ggtitle("Total Damage Dealt To Champions Per Minute \n Over the Last 5 Games")
+    return tddtc
+
+def graph_mddtc(df):
+    mddtc = ggplot(df) + aes(x='games_ago', y='MagicDamageDealtToChampionsPerMinute') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('Magic Damage Dealt To Champions \n Per Minute') + ggtitle("Magic Damage Dealt To Champions Per Minute \n Over the Last 5 Games")   
+    return mddtc
+
+def graph_pddtc(df):
+    pddtc = ggplot(df) + aes(x='games_ago', y='PhysicalDamageDealtToChampionsPerMinute') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('Physical Damage Dealt To Champions \n Per Minute') + ggtitle("Physical Damage Dealt To Champions Per Minute \n Over the Last 5 Games")
+    return pddtc
+
+def graph_gepm(df):
+    gepm = ggplot(df) + aes(x='games_ago', y='goldEarnedPerMinute') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('Gold Earned Per Minute') + ggtitle("Gold Earned Per Minute \n Over the Last 5 Games")
+    return gepm
+
+def graph_tmk(df):
+    tmk = ggplot(df) + aes(x='games_ago', y='totalMinionsKilledPerMinute') + geom_line(color="black", linetype='dashed') + geom_point(aes(color='lane', size=3), show_legend={'size': False}) + scale_x_reverse() + geom_smooth(method = "lm") + xlab('Games Ago') + ylab('Total Minions Killed \n Per Minute') + ggtitle("Total Minions Killed Per Minute \n Over the Last 5 Games")
+    return tmk
+
+@api.route('/img/<summoner>')
+def send_img(summoner):
+    return send_from_directory('./static/images', f'{summoner}_kda.jpg')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
