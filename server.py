3c1c948daf98b4d6060b58e2527e368ac7a1518d
angsabrina <angsabrina98@yahoo.com>
2020-11-08 08:46:06 -0800

    change host for heroku

--- server.py
+++ server.py
@@ -431,5 +431,5 @@ def send_img(summoner_postfix):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # api.run(host='0.0.0.0', port=port)
-    api.run(debug=True, port=port)
+    api.run(host='0.0.0.0', port=port)
+    # api.run(debug=True, port=port)
